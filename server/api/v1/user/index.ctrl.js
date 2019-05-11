3c745b44a6886062a670580be04473e454d8785e
Hosung Lim <holnet1026@gmail.com>
2019-05-11 12:13:00 +0900

    fix: 🔥 resolve merge conflict

--- server/api/v1/user/index.ctrl.js
+++ server/api/v1/user/index.ctrl.js
@@ -1,5 +1,28 @@
-export const getUserProfile = (req, res) => {
-  res.json({
-    ok: true,
-  });
+import axios from 'axios';
+import camelcase from 'camelcase-keys';
+
+export const getUserProfile = async (req, res) => {
+  try {
+    const user = req.params.user;
+    const response = await axios.get(`https://api.github.com/users/${user}`);
+    const arr = camelcase(response.data);
+    res.json(arr);
+  } catch (e) {
+    console.log(e);
+  }
+  res.json({});
+};
+
+export const getUserIssue = async (req, res) => {
+  try {
+    const owner = req.params.owner;
+    const repo = 'react-study';
+    const response = await axios.get(
+      `https://api.github.com/repos/${owner}/${repo}/issues`,
+    );
+    const arr = camelcase(response.data, { deep: true });
+    res.json(arr);
+  } catch (err) {
+    console.log(err);
+  }
 };
