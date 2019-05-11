e9855c9270f5cdcd076d82c3fb5cc2e09f39c2db
Q00 <jqyu.lee@gmail.com>
2019-05-11 11:48:22 +0900

    feat: ⚡️ add passport github module

---
+++ server/lib/github.js
@@ -0,0 +1,45 @@
+import githubStrategy from 'passport-github';
+import dotenv from 'dotenv';
+import path from 'path';
+import User from '../models/user';
+
+const config = dotenv.config({
+  path: path.join(
+    __dirname,
+    `../.env.${process.env.NODE_ENV || 'development'}`,
+  ),
+});
+console.log(config.parsed);
+const github = githubStrategy.Strategy;
+
+module.exports = (app, passport) => {
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.use(
+    new github(
+      {
+        clientID: config.parsed.GITHUB_CLIENT_ID,
+        clientSecret: config.parsed.GITHUB_CLIENT_SECRET,
+        callbackURL: config.parsed.GITHUB_CALLBACK_URL,
+        scope: 'user:email',
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        const userId = profile.id;
+        const userName = profile.username;
+        console.log(profile);
+        return done(null, profile);
+        //디비 연결
+        //있으면 로그인, 없으면 회원가입
+      },
+    ),
+  );
+};
