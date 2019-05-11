4fe02b6a6a8e6084c806ca862c4a859626326e35
Hosung Lim <holnet1026@gmail.com>
2019-05-11 15:19:47 +0900

    feat: ⚡️ implement UserPost

---
+++ components/UserPost/UserPost.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import styled from 'styled-components';
+import TagList from '../TagList';
+import dayjs from 'dayjs';
+import { MdSchedule } from 'react-icons/md';
+
+const UserPostWrapper = styled.div``;
+
+const PostList = styled.ul``;
+
+const PostContent = styled.div`
+  display: flex;
+`;
+
+const PostImage = styled.img`
+  border-radius: 3px;
+  width: 200px;
+  height: 150px;
+  margin-right: 10px;
+`;
+
+const PostListItem = styled.li`
+  display: flex;
+  padding-bottom: 20px;
+  border-bottom: 1px solid #ddd;
+  margin-bottom: 20px;
+`;
+
+const PostHeading = styled.h1`
+  font-size: 24px;
+  margin-bottom: 10px;
+`;
+
+const PostDescription = styled.p`
+  margin-bottom: 10px;
+  color: #4b4b4b;
+  font-size: 12px;
+  line-height: 18px;
+`;
+
+const HelpBlock = styled.span`
+  color: #4b4b4b;
+  font-size: 12px;
+  display: inline-flex;
+  align-items: center;
+  margin-top: 5px;
+`;
+
+const UserPost = ({ posts }) => (
+  <UserPostWrapper>
+    <PostList>
+      {posts &&
+        posts.map((post) => (
+          <PostListItem>
+            <PostContent>
+              <PostImage src={post.thumbnail} />
+              <div>
+                <PostHeading>{post.title}</PostHeading>
+                <PostDescription>{`${post.plainBody.slice(
+                  0,
+                  200,
+                )}...`}</PostDescription>
+                {post.labels.length > 0 && <TagList tags={post.labels} />}
+                <HelpBlock>
+                  <MdSchedule />
+                  <span style={{ marginLeft: '3px' }}>
+                    {dayjs(post.createdAt).format('YYYY.MM.DD')}
+                  </span>
+                </HelpBlock>
+              </div>
+            </PostContent>
+          </PostListItem>
+        ))}
+    </PostList>
+  </UserPostWrapper>
+);
+
+export default UserPost;
