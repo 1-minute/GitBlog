0f78bf992087c6844d3e09fffb383254d75a32bf
Q00 <jqyu.lee@gmail.com>
2019-05-12 01:08:03 +0900

    feat: ⚡️ add getReadme module

--- server/api/v1/user/index.js
+++ server/api/v1/user/index.js
@@ -4,6 +4,7 @@ import {
   getUserIssue,
   getUserRepositories,
   saveUserRepository,
+  aboutReadMe,
 } from './index.ctrl';
 
 const router = new Router();
@@ -12,4 +13,6 @@ router.get('/:user/profile', getUserProfile);
 router.get('/:user/repos', getUserRepositories);
 router.get('/:user/issues', getUserIssue);
 router.post('/:user/repos', saveUserRepository);
+router.get(`/:user/about`, aboutReadMe);
+
 export default router;
