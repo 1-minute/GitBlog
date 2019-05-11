df6b91cacdf57c7e85cea7cc43deb7b706a70f1a
Hosung Lim <holnet1026@gmail.com>
2019-05-11 21:40:59 +0900

    chore: 📦 eslint rule setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,26 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es6": true
+  env: {
+    browser: true,
+    es6: true,
   },
-  "extends": ["eslint:recommended", "airbnb", "eslint-config-prettier"],
-  "globals": {
-    "Atomics": "readonly",
-    "SharedArrayBuffer": "readonly"
+  extends: ['airbnb', 'eslint-config-prettier'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
-  "parserOptions": {
-    "ecmaFeatures": {
-        "jsx": true
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "ecmaVersion": 2018,
-    "sourceType": "module"
+    ecmaVersion: 2018,
+    sourceType: 'module',
   },
-  "plugins": [
-    "react"
-  ],
-  "rules": {
-  }
+  plugins: ['react'],
+  rules: {
+    'react/prop-types': 0,
+    'import/order': 0,
+    'no-param-reassign': 0,
+    'import/no-extraneous-dependencies': 0,
+    'import/prefer-default-export': 0,
+  },
 };
