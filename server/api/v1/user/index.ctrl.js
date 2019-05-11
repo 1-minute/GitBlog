e5a9da13066fe1bdc2f43f48693d653085d31b28
Q00 <jqyu.lee@gmail.com>
2019-05-11 13:38:47 +0900

    fix: 🔥 add ratelimit parameter

--- server/api/v1/user/index.ctrl.js
+++ server/api/v1/user/index.ctrl.js
@@ -1,10 +1,13 @@
 import axios from 'axios';
 import camelcase from 'camelcase-keys';
+import getWhateverParameter from '../../../util/whateverParameter';
 
 export const getUserProfile = async (req, res) => {
   try {
     const user = req.params.user;
-    const response = await axios.get(`https://api.github.com/users/${user}`);
+    const response = await axios.get(
+      getWhateverParameter(`https://api.github.com/users/${user}`),
+    );
     const arr = camelcase(response.data);
     res.json(arr);
   } catch (e) {
@@ -17,8 +20,11 @@ export const getUserIssue = async (req, res) => {
   try {
     const owner = req.params.owner;
     const repo = 'react-study';
+    await getWhateverParameter;
     const response = await axios.get(
-      `https://api.github.com/repos/${owner}/${repo}/issues`,
+      getWhateverParameter(
+        `https://api.github.com/repos/${owner}/${repo}/issues`,
+      ),
     );
     const arr = camelcase(response.data, { deep: true });
     res.json(arr);
